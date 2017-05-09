36f4ecb51d6f4dcd7bc6e1763d1d1ebd9e9bf1ad
borysn <xborysn@gmail.com>
2017-05-09 03:41:53 -0500

    update header

--- eprl/eprl.py
+++ eprl/eprl.py
@@ -1,13 +1,21 @@
 #!/bin/env python3
-#
 # eprl.py
 # author: borysn | oxenfree
 # license: MIT
 #
 # edit portage resume list
 #
-# usage: eprl.py [-h] [-l] [-r ITEMNUM] [-b] [-v]
+# usage: eprl.py [-h] [-l] [-r ITEMNUMS [ITEMNUMS ...]] [-a ITEMS [ITEMS ...]] [-b] [-v]
 #
+# optional arguments:
+#   -h, --help            show this help message and exit
+#   -l, --list            list portage resume items
+#   -r ITEMNUMS [ITEMNUMS ...], --remove ITEMNUMS [ITEMNUMS ...]
+#                         remove portage resume items
+#   -a ITEMS [ITEMS ...], --add ITEMS [ITEMS ...]
+#                         add resume item(s) to a resume list
+#   -b, --backup          perform operations on backup list
+#   -v, --version         show program's version number and exit
 import sys
 import util, vargs, dbstore, resolver
 from colors import tcolor, bcolors
