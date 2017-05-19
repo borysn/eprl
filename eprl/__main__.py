555aa06bf80397786d85f261140588e125ca6cab
borysn <xborysn@gmail.com>
2017-05-18 22:31:32 -0500

    minimize usage info for header comment

--- eprl/__main__.py
+++ eprl/__main__.py
@@ -5,18 +5,7 @@
 # license: MIT
 #
 # edit portage resume list
-#
-# usage: eprl.py [-h] [-l] [-r ITEMNUMS [ITEMNUMS ...]] [-a ITEMS [ITEMS ...]] [-b] [-v]
-#
-# optional arguments:
-#   -h, --help            show this help message and exit
-#   -l, --list            list portage resume items
-#   -r ITEMNUMS [ITEMNUMS ...], --remove ITEMNUMS [ITEMNUMS ...]
-#                         remove portage resume items
-#   -a ITEMS [ITEMS ...], --add ITEMS [ITEMS ...]
-#                         add resume item(s) to a resume list
-#   -b, --backup          perform operations on backup list
-#   -v, --version         show program's version number and exit
+#     $ eprl -h
 import sys
 from eprl import util, vargs, dbstore, resolver
 from eprl.colors import tcolor
